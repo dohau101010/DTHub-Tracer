c1864cbba5d3b7f490dd581233b1c34b02e53115
Do Trung Hau <157142819+dohau101010@users.noreply.github.com>
2025-02-27 22:32:44 +0700

    Update report.js

--- report.js
+++ report.js
@@ -1578,14 +1578,14 @@ function updateSubVideo10xxx() {
 `;
 }
 // ......................................................................................................
-document
-  .getElementById("traceBoxLink")
-  .addEventListener("click", function (event) {
-    event.preventDefault(); // Ngăn chặn chuyển trang mặc định
+// document
+//   .getElementById("traceBoxLink")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault(); // Ngăn chặn chuyển trang mặc định
 
-    if (userName === "Do Trung Hau" || userName === "Vũ An") {
-      window.location.href = "infor.html"; // Chuyển hướng nếu user hợp lệ
-    } else {
-      alert("User account does not have permission to access this page!");
-    }
-  });
+//     if (userName === "Do Trung Hau" || userName === "Vũ An") {
+//       window.location.href = "infor.html"; // Chuyển hướng nếu user hợp lệ
+//     } else {
+//       alert("User account does not have permission to access this page!");
+//     }
+//   });
